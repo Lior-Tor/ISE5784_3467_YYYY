399dcb6eb9f4462f48a50a5f24a44960e41aa8e0
Lior-Tor <tordjmanlior1000@hotmail.com>
2024-05-15 19:37:20 +0300

    First commit

---
+++ src/primitives/Vector.java
@@ -0,0 +1,57 @@
+package primitives;
+
+import static primitives.Util.alignZero;
+
+public class Vector extends Point {
+    public Vector(double x, double y, double z) {
+        super(x, y, z);
+    }
+
+    public Vector(Double3 xyz) {
+        super(xyz);
+    }
+
+    @Override
+    public String toString() {
+        return "Vector{" + xyz + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Vector vector)) return false;
+
+        return xyz.equals(vector.xyz);
+    }
+
+    public Vector add(Vector vector) {
+        return new Vector(xyz.add(vector.xyz));
+    }
+
+    public Vector scale(double scalar) {
+        return new Vector(xyz.scale(scalar));
+    }
+
+    public double lengthSquared() {
+        return xyz.d1;
+    }
+
+    public double length() {
+        return Math.sqrt(lengthSquared());
+    }
+
+    public Vector normalize() {
+        double length = alignZero(length());
+        if (length == 0)
+            throw new ArithmeticException("Cannot normalize Vector(0,0,0)");
+        return new Vector(xyz.scale(1 / length));
+    }
+
+    public Vector crossProduct(Vector vector) {
+
+    }
+
+    public double dotProduct(Vector vector) {
+
+    }
+}
