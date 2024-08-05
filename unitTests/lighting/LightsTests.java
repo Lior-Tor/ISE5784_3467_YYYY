3383160f6afdba0ff64fbfe1e832b28fa9c25856
Lior-Tor <tordjmanlior1000@hotmail.com>
2024-08-06 01:12:40 +0300

    PR07

--- unitTests/lighting/LightsTests.java
+++ unitTests/lighting/LightsTests.java
@@ -1,6 +1,8 @@
 package lighting;
 
 import static java.awt.Color.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
